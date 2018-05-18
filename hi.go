ec4c3f551903ec830cba2f69be0d39559fbe6663
MJ <michaeljefferies08@gmail.com>
2018-05-18 22:16:18 +0100

    Daily reminder that you must import a print statement in go

---
+++ hi.go
@@ -0,0 +1,13 @@
+package main 
+import (
+	f "fmt"
+	t "time"
+)
+
+func main() {
+	f.Println("Ooga booga")
+
+	f.Println("The time you got ooga booga'd was: ", t.Now())
+
+	f.Println("Imagine having to import in order to print wow wtf google")
+}
